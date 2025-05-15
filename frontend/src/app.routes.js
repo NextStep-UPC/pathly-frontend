ce7c0bab16fd1327795066f183cc1cab9340fdba
vquijandria <u201822697@upc.edu.pe>
2025-05-15 00:50:37 -0500

    feat(app.routes.js): uncommented the students path

--- frontend/src/app.routes.js
+++ frontend/src/app.routes.js
@@ -2,14 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import { authRoutes } from './app/auth/auth.routes.js';
 import { publicRoutes } from './app/public/public.routes.js';
-// import { studentRoutes } from './app/student/student.routes.js';
+ import { studentRoutes } from './app/student/student.routes.js';
 // import { psychologistRoutes } from './app/psychologist/psychologist.routes.js';
 // import { adminRoutes } from './app/admin/admin.routes.js';
 
 const routes = [
   ...publicRoutes,      // Landing, ayuda, contacto
   ...authRoutes,        // Login, registro
-  // ...studentRoutes,  // Test, resultados, citas
+  ...studentRoutes,  // Test, resultados, citas
   // ...psychologistRoutes, // Panel del orientador
   // ...adminRoutes,    // Dashboard administrativo
 
