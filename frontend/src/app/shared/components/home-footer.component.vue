fe92702b4db0ad0e41e273f945022b2e899368f2
rhylow <leccapedro8@gmail.com>
2025-05-15 23:30:02 -0500

    feat: update home-footer.component.vue

--- frontend/src/app/shared/components/home-footer.component.vue
+++ frontend/src/app/shared/components/home-footer.component.vue
@@ -1,37 +1,44 @@
 <template>
-  <header class="header">
-    <div class="header-container">
-      <img :src="logo" alt="Pathly Logo" class="logo" />
-
-      <div class="auth-buttons">
-        <RouterLink to="/login" class="btn login-btn">{{ $t('navbar.login') }}</RouterLink>
-        <RouterLink to="/register" class="btn register-btn">{{ $t('navbar.register') }}</RouterLink>
-        <LanguageSwitcher />
+  <footer class="footer">
+    <div class="footer-container">
+      <div class="footer-logo">
+        <img :src="logo" alt="Pathly Logo" />
+        <p>© Pathly 2025<br />{{ $t('footer.rights') }}</p>
       </div>
 
-      <button class="hamburger" @click="$emit('toggleMenu')" id="hamburger">☰</button>
+      <div class="footer-links">
+        <div class="footer-column">
+          <h4>{{ $t('footer.explore') }}</h4>
+          <ul>
+            <li><a href="#">{{ $t('footer.feedback') }}</a></li>
+            <li><a href="#">{{ $t('footer.faq') }}</a></li>
+            <li><a href="#">{{ $t('footer.terms') }}</a></li>
+            <li><a href="#">{{ $t('footer.privacy') }}</a></li>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <h4>{{ $t('footer.social') }}</h4>
+          <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Tik Tok</a></li>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <h4>{{ $t('footer.help') }}</h4>
+          <ul>
+            <li><a href="#">{{ $t('footer.contact') }}</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-  </header>
+  </footer>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
-import LanguageSwitcher from './language-switcher.component.vue';
-
-defineProps({
-  isMenuOpen: Boolean
-});
-
 const logo = new URL('@/assets/images/pathly-logo.png', import.meta.url).href;
 </script>
 
 <style scoped>
 @import '../../shared/styles/home.css';
-
-.auth-buttons {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  margin-left: auto;
-}
 </style>
