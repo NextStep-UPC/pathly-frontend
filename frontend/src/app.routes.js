87f47d2ee603da9328330533a4be4bc778158e9b
rhylow <leccapedro8@gmail.com>
2025-05-16 07:43:55 -0500

    feat: update app.routes.js

--- frontend/src/app.routes.js
+++ frontend/src/app.routes.js
@@ -4,14 +4,14 @@ import { authRoutes } from './app/auth/auth.routes.js';
 import { publicRoutes } from './app/public/public.routes.js';
 import { studentRoutes } from './app/student/student.routes.js';
 import { psychologistRoutes } from './app/psychologist/psychologist.routes.js';
-// import { adminRoutes } from './app/admin/admin.routes.js';
+import { adminRoutes } from './app/admin/admin.routes.js';
 
 const routes = [
   ...publicRoutes,      // Landing, ayuda, contacto
   ...authRoutes,        // Login, registro
   ...studentRoutes,  // Test, resultados, citas
   ...psychologistRoutes, // Panel del orientador
-  // ...adminRoutes,    // Dashboard administrativo
+  ...adminRoutes,    // Dashboard administrativo
 
   { path: '/:pathMatch(.*)*', redirect: '/' }
 ];
