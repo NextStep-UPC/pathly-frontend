55ecc8c52275d491366f085aaea7e255cb7b8ada
rhylow <leccapedro8@gmail.com>
2025-05-16 07:44:34 -0500

    feat: add admin.routes.js

---
+++ frontend/src/app/admin/admin.routes.js
@@ -0,0 +1,9 @@
+import SessionsScheduleView from './views/sessions-schedule-view.vue';
+
+export const adminRoutes = [
+    {
+        path: '/admin',
+        name: 'SessionsSchedule',
+        component: SessionsScheduleView,
+    },
+];
